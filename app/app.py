3705cfd0a55f9ec5a17b2748b0b0728dab887672
Lauro Gomes <lagomes@redhat.com>
2022-06-25 14:36:53 -0300

    Autenticação

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import uvicorn
+import secrets
 
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
@@ -10,8 +11,25 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
 from subprocess import Popen
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-app=FastAPI()
+
+ENVIROMENT_VERSION = os.getenv('ENVIROMENT')
+
+app=FastAPI(
+    title='Python FASTAPI deployment LAB',
+    version=ENVIROMENT_VERSION+' 0.1.10',
+    docs_url = None,
+    redoc_url = None,
+    contact={
+        "name": "Lauro de Paula Gomes",
+        "url": "https://laurobmb.wordpress.com/",
+        "email": "laurobmb@gmail.com",
+    }
+)
+
+security = HTTPBasic()
 
 path_to_output_file = '/tmp/index.html'
 myoutput = open(path_to_output_file,'w')
@@ -19,7 +37,6 @@ myoutput = open(path_to_output_file,'w')
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-ENVIROMENT_VERSION = os.getenv('ENVIROMENT')
 
 class ModeloBase(BaseModel):
     usuario: str
@@ -103,13 +120,13 @@ def indexhtml():
 
 
 @app.get("/")
-async def returnindex():
+async def index():
     indexhtml()
     return FileResponse('pages/index.html')
 
 
 @app.get("/json", response_model=ModeloBase)
-async def modelo001():
+async def ModeloBase():
     return ModeloBase(
         usuario = "laurobmb",
         nome = "Lauro",
@@ -120,7 +137,7 @@ async def modelo001():
 
 
 @app.get("/info", response_model=infos)
-async def infos_web():
+async def pod_infos():
     return infos(
         date = date(),
         arquivos = lss(),
@@ -130,15 +147,15 @@ async def infos_web():
 
 
 @app.get("/page")
-async def returnindex():
+async def pagina():
     indexhtml()
     return FileResponse('pages/page1.html')
 
 
 @app.get("/version")
-async def read_root():
+async def version():
     version = f"{sys.version_info.major}.{sys.version_info.minor}"
-    message = f"FastAPI rodando em Uvicorn. Usando Python de {ENVIROMENT_VERSION} {version}"
+    message = f'FastAPI rodando em Uvicorn. Python version: {version} Ambiente: {ENVIROMENT_VERSION}'
     return {"message": message}
 
 
@@ -146,6 +163,47 @@ async def read_root():
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
+def checkFileSecret():
+    import os.path
+    secret_user_file="/opt/secrets/username" 
+    secret_pass_file="/opt/secrets/password"
+    secret_user_file_file_exists = os.path.exists(secret_user_file)
+    secret_pass_file_file_exists = os.path.exists(secret_pass_file)
+
+    if secret_user_file_file_exists:
+        secret_user = open(secret_user_file,"r")
+    else:
+        secret_user = "error"
+
+    if secret_pass_file_file_exists:
+        secret_pass = open(secret_pass_file,"r")
+    else:
+        secret_pass = "error"
+
+    return secret_user,secret_pass
+
+    #configmap = open("/opt/config/page","r")
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    secret_user,secret_pass = checkFileSecret()
+    correct_username = secrets.compare_digest(credentials.username, secret_user)
+    correct_password = secrets.compare_digest(credentials.password, secret_pass)
+    if not (correct_username and correct_password) or secret_user is 'error':
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/users")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8080,
+        debug=False, 
+        log_level="info")
